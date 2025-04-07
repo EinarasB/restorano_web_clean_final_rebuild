70f4bb02b793c1039ff56469a5080325cba8ba7f
EinarasB <bargaila99@gmail.com>
2025-04-07 20:29:50 +0300

    Update run.py
    Pridėtas PORT nustatymas Run.py faile

--- run.py
+++ run.py
@@ -1,9 +1,10 @@
-
+﻿import os
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from fastapi.middleware.cors import CORSMiddleware
 from app.routes import router as register_router
 from app.models import Base, engine
+import uvicorn
 
 Base.metadata.create_all(bind=engine)
 
@@ -23,3 +24,7 @@ app.include_router(register_router)
 @app.get("/")
 def read_root():
     return {"message": "Restoranas AI veikia!"}
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8000))  # <- Šita eilutė yra būtina Render
+    uvicorn.run("run:app", host="0.0.0.0", port=port, reload=False)
