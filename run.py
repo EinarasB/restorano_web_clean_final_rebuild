d884062093fe163f1e5d84afa14bf2d7c1d9f25a
EinarasB <bargaila99@gmail.com>
2025-04-07 20:38:46 +0300

    Update run.py
    update

--- run.py
+++ run.py
@@ -30,5 +30,8 @@ def homepage(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8000))  # <- Šita eilutė yra būtina Render
-    uvicorn.run("run:app", host="0.0.0.0", port=port, reload=False)
+    import uvicorn
+    import os
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("run:app", host="0.0.0.0", port=port)
+
