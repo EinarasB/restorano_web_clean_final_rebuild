9b60151ae0f557a97b49b2197e79b78ad9456fbe
EinarasB <bargaila99@gmail.com>
2025-04-16 20:04:30 +0300

    aa
    aa

--- app/routes.py
+++ app/routes.py
@@ -16,7 +16,6 @@ openai_api_key = os.getenv("OPENAI_API_KEY")
 client = OpenAI(api_key=openai_api_key)
 
 # ✅ ČIA VIENAS router
-router = APIRouter()
 templates = Jinja2Templates(directory="app/templates")
 
 # ======== AI CHAT ==========
