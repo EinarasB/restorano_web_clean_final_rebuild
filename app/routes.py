708784d24e46ceb673d2df75b2d64589816f12c7
EinarasB <bargaila99@gmail.com>
2025-04-16 20:15:22 +0300

    aa
    aa

--- app/routes.py
+++ app/routes.py
@@ -15,6 +15,9 @@ load_dotenv()
 openai_api_key = os.getenv("OPENAI_API_KEY")
 client = OpenAI(api_key=openai_api_key)
 
+print("🔐 API KEY:", openai_api_key)
+
+
 # ✅ ČIA VIENAS router
 router = APIRouter()
 templates = Jinja2Templates(directory="app/templates")
---
+++ test.py
@@ -0,0 +1,13 @@
+﻿from openai import OpenAI
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+
+response = client.chat.completions.create(
+    model="gpt-4o",
+    messages=[{"role": "user", "content": "Ką rekomenduotum iš meniu?"}]
+)
+
+print(response.choices[0].message.content)
