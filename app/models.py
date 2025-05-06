4b0931b0cc38b8e1def6ddd9aaac084d0f27b19d
EinarasB <bargaila99@gmail.com>
2025-05-06 18:30:27 +0300

    a
    a

--- app/models.py
+++ app/models.py
@@ -3,6 +3,9 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship, sessionmaker
 from datetime import datetime
 import os
+from dotenv import load_dotenv
+load_dotenv()
+
 
 DATABASE_URL = os.environ.get("DATABASE_URL")
 engine = create_engine(DATABASE_URL)
