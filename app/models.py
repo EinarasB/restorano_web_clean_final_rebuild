37f8235e6d3af32001a3745aeb8feaa57ae230ed
EinarasB <bargaila99@gmail.com>
2025-05-08 19:36:36 +0300

    a
    a

--- app/models.py
+++ app/models.py
@@ -1,4 +1,4 @@
-﻿from sqlalchemy import Column, Integer, String, ForeignKey, Float, create_engine, DateTime
+﻿from sqlalchemy import Column, Integer, String, ForeignKey, Float, create_engine, DateTime, Text
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship, sessionmaker
 from datetime import datetime
